1491ecc91adcae12285da670c930a429868958d5
Luy Vannda <philips.joe329@gmail.com>
2024-01-26 15:29:03 +0700

    learn to use shadcn-ui and lucide react icons

--- src/app/account/page.jsx
+++
@@ -1,9 +0,0 @@
-import { SAuthForm } from '@/components/auth';
-
-export default function AccountPage() {
-  return (
-    <div>
-      <SAuthForm />
-    </div>
-  );
-}
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,16 +1,20 @@
 import Link from 'next/link';
+import { buttonVariants } from '@/components/ui/button';
+import { Store } from 'lucide-react';
 
 export default function NavBar() {
   return (
     <div className="w-full bg-gray-400 text-white z-10">
-      <nav className="container relative flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link href="/" className="font-bold text-3xl">
-          Logo
+      <nav className="container relative flex flex-wrap items-center justify-between mx-auto px-4 py-2">
+        <Link href="/" className="">
+          <Store />
         </Link>
         <div className="space-x-4 text-xl">
           <Link href="/products">Products</Link>
           <Link href="#">Search</Link>
-          <Link href="/account">Account</Link>
+          <Link href="/account" className={buttonVariants()}>
+            Sign in
+          </Link>
           <Link href="/contactus">Contact Us</Link>
           <Link href="#">Cart</Link>
         </div>
